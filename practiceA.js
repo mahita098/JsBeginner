036f09dcdbe855db684fb264dc1ec6ff2ca230a8
Mahita Ramgolam <94129495+mahita098@users.noreply.github.com>
2023-01-10 12:40:26 +0400

    preactice A guest implementation

--- practiceA.js
+++ practiceA.js
@@ -10,7 +10,7 @@ let plus1 = document.getElementById("plus1")
 let plusOne = 1
 
 function add1() {
-    console.log("add clcked")
+    console.log("add clicked")
     let sum = num1 + plusOne
     result.textContent = "result:" + sum
 }
@@ -23,6 +23,45 @@ function add2() {
     result.textContent = "result:" + sum
 }
 
+let minus3 = document.getElementById("minus3")
+let minusThree = 3
+
+function min3() {
+    let sum = num1 - minusThree
+    result.textContent = "result:" + sum
+}
+
+
+let plus4 = document.getElementById("plus4")
+
+let plusFour = 4
+
+function add4() {
+    
+    let sum = num2 + plusFour
+    result.textContent = "result:" + sum
+}
+
+let plus5 = document.getElementById("plus5")
+let plusFive = 5
+
+function add5() {
+    let sum = num2 + plusFive
+    result.textContent = "result:" + sum
+} 
+
+let minus1 = document.getElementById("minus1")
+let minusOne = 1
+
+function min1() {
+    let sum = num2 - minusOne
+    result.textContent = "result:" + sum
+}
+
 
 add1()
 add2()
+min3()
+add4()
+add5()
+min1()
