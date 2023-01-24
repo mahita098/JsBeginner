63407676931a8f1e7a1f528c510d44a80aef365a
Mahita Ramgolam <94129495+mahita098@users.noreply.github.com>
2023-01-24 13:14:44 +0400

    ex15 implementation

--- ex14/ex14.js
+++ ex14/ex14.js
@@ -1,15 +1,16 @@
 let arr = [];
 
 function add_element_to_array() {
-  let numberToAdd = Math.random(0, 10); //document.getElementById("input").value;
+  let numberToAdd = document.getElementById("input").value;
   arr.push(numberToAdd);
 
   let result = document.getElementById("Result");
-  let total = 0;
+  // let total = 0;
 
-  arr.map(function (item) {
-    total = total + parseFloat(item);
-  });
+  // arr.map(function (item) {
+  //   total = total + parseFloat(item);
+  // });
 
-  result.innerHTML = total;
+  // result.innerHTML = total;
+  result.innerHTML = arr;
 }
